d3babab90a7ede26ccb5c094fee5260e7b8eea80
Nathaniel Bibler <nate@envylabs.com>
2010-02-22 00:14:54 -0500

    Added explicit requirement for friendly_id version 2.3.2

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem        'friendly_id'
+  config.gem        'friendly_id', :version => '=2.3.2'
 
   config.time_zone  = 'UTC'
 end
