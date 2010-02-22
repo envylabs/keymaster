2078d4e0535b1c9e787b238ac1c176b5e8cc43a1
Nathaniel Bibler <nate@envylabs.com>
2010-02-22 00:01:12 -0500

    Added User end points

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  
+  def index
+    @project  = Project.find(params[:project_id])
+    @users    = @project.users
+    
+    respond_to do |format|
+      format.yaml { render :text => @users.to_yaml }
+    end
+  end
+  
+  def show
+    @parent   = params.has_key?(:project_id) ? Project.find(params[:project_id]).users : User
+    @user     = @parent.find_by_login(params[:id]) || raise(ActiveRecord::RecordNotFound)
+    
+    respond_to do |format|
+      format.yaml { render :text => @user.to_yaml }
+    end
+  end
+  
 end
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,8 +1,74 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  context 'The UsersController' do
+    
+    context 'using GET to index' do
+      
+      setup do
+        @project = Factory(:project)
+        Factory(:membership, :project => @project)
+        Factory(:membership, :project => @project)
+        get :index, :project_id => @project.to_param
+      end
+      
+      should_respond_with               :success
+      should_respond_with_content_type  :yaml
+      should_assign_to(:users)          { @project.users }
+      
+    end
+    
+    context 'using GET to show' do
+      
+      context 'directly' do
+        
+        setup do
+          @user       = Factory(:user)
+          get :show, :id => @user.to_param
+        end
+
+        should_respond_with               :success
+        should_respond_with_content_type  :yaml
+        should_assign_to(:user)           { @user }
+        
+      end
+      
+      context 'for a project' do
+        
+        context 'for a user of the project' do
+          
+          setup do
+            @project    = Factory(:project)
+            @user       = Factory(:user)
+            @membership = Factory(:membership, :project => @project, :user => @user)
+            get :show, :project_id => @project.to_param, :id => @user.to_param
+          end
+
+          should_respond_with               :success
+          should_respond_with_content_type  :yaml
+          should_assign_to(:user)           { @user }
+          
+        end
+        
+        context 'for a user not in the project' do
+          
+          setup do
+            @request.remote_addr = '1.2.3.4'
+            @project    = Factory(:project)
+            @user       = Factory(:user)
+            @membership = Factory(:membership, :project => @project)
+            get :show, :project_id => @project.to_param, :id => @user.to_param
+          end
+
+          should_respond_with               :not_found
+          
+        end
+        
+      end
+      
+    end
+    
   end
+  
 end
