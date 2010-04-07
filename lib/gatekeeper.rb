67efcf4c12d3153738feb43cfe6b11ddd39b5d01
Nathaniel Bibler <git@nathanielbibler.com>
2010-04-07 13:16:53 -0400

    Cleaned up the gatekeeper and added deploy user ssh key management.  Version bump.

--- lib/gatekeeper.rb
+++ lib/gatekeeper.rb
@@ -4,7 +4,7 @@
 #   Load and maintain all users for the 'envy-labs' project:
 #     PROJECT=envy-labs ruby gatekeeper.rb
 #   Normally, this would be done from a cron task:
-#     */5 * * * * PROJECT=envy-labs ruby gatekeeper.rb
+#     */5 * * * * PROJECT=envy-labs /root/gatekeeper.rb &>/dev/null
 #
 
 ENV['PATH'] = '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
@@ -16,18 +16,6 @@ require 'uri'
 require 'yaml'
 require 'cgi'
 
-module ExecutionResult
-  
-  def success?
-    @success || false
-  end
-  
-  def success=(value)
-    @success = value
-  end
-  
-end
-
 HTTP_ERRORS = [ Timeout::Error,
                 Errno::EINVAL,
                 Errno::ECONNRESET,
@@ -36,38 +24,54 @@ HTTP_ERRORS = [ Timeout::Error,
                 Net::HTTPHeaderSyntaxError,
                 Net::ProtocolError          ]     unless defined?(HTTP_ERRORS)
 
-##
-# Execute a file on the server with optional parameters.
-# 
-def execute(executable, options = {})
-  executable_path = `/usr/bin/env which "#{executable}"`.strip
-  if $?.success?
-    result = `\"#{executable_path}\" #{options[:parameters]}`.strip
-    result.extend(ExecutionResult)
-    result.success = $?.success?
-    log(%|Execution of "#{executable_path}" #{options[:parameters]} failed|, :fail => options[:fail]) unless result.success?
-    result
-  else
-    log(%|Could not locate "#{executable}" in the user's environment|, :fail => true)
+module Keymaster
+  
+  ##
+  # Returns the version of the Keymaster in which this Gatekeeper instance
+  # is compatible against.
+  # 
+  def self.version
+    @_version ||= '%CURRENT_KEYMASTER_VERSION%'
   end
-end
-
-##
-# Log a message to syslog.
-# 
-def log(message, options = {})
-  execute("logger", :parameters => %|-i -t "Gatekeeper" "#{message}"|)
-  exit(1) if options[:fail]
-end
-
-##
-# Envy Labs ShellUsers
-#
-class ShellUser
   
-  Version = '%CURRENT_KEYMASTER_VERSION%'
+  ##
+  # Returns a collection of Users allowed to access the requested project.
+  # 
+  def self.users_for_project(project)
+    yaml  = query("http://keymaster.envylabs.com/projects/#{project}/users.yaml")
+    YAML.load(yaml).collect { |user_data| ShellUser.new(user_data) }
+  end
   
-  PublicKey = <<-KEY
+  ##
+  # Returns +true+ if the given content (with it's associated signature) came
+  # from the Keymaster.  This method performs an RSA signature verification
+  # against the pre-shared public key.
+  # 
+  def self.valid?(content, signature)
+    rsa.verify(digest, signature, content)
+  end
+  
+  
+  private
+  
+  
+  def self.rsa
+    @_rsa ||= OpenSSL::PKey::RSA.new(public_key)
+  rescue OpenSSL::PKey::RSAError
+    log("Invalid pre-shared RSA public key.  Aborting.")
+    exit(1)
+  end
+  
+  def self.digest
+    @_rsa_digest ||= OpenSSL::Digest::SHA256.new
+  end
+  
+  ##
+  # Returns the RSA Public Key for the Keymaster.  All data collected from
+  # the Keymaster should be signed with the companion RSA Private Key.
+  # 
+  def self.public_key
+    @_public_key ||= <<-KEY
 -----BEGIN RSA PUBLIC KEY-----
 MIIBCgKCAQEA3qENRm18NmxUPQ8KFUbZdRyc7aQgSfnzgn3a5a1VYVOBo+KuuOyN
 o7U/j9ICy7NFqsNTF91/BDJKiApQOd63l9aS7xp7KzpOferjnFqrDCcOfxgNpy3T
@@ -77,136 +81,258 @@ qPvCNQw4+/t2QAvr1IWUKgXkO4e5Rw4o2bglTW23akyXCCW+f0LMMXOHzaiDBtzd
 oqzuY9HMI8ue9NmGPxahF3pHxnnBhBZojwIDAQAB
 -----END RSA PUBLIC KEY-----
 KEY
-  
-  def self.manage(project)
-    yaml = data_for(project)
-    user_attributes = YAML.load(yaml).freeze
-    add_users(user_attributes)
-    remove_unlisted_users(user_attributes)
-  rescue *HTTP_ERRORS
-    log("HTTP Exception - #{$!.class.name} - #{$!.message}", :fail => true)
   end
   
-  def self.add_users(attributes)
-    attributes.map { |a| ShellUser.new(a).setup! }
-  end 
-  
-  def self.remove_unlisted_users(attributes)
-    local_logins = execute("cat", :parameters => %|/etc/group \| grep "^envylabs_accounts"|, :fail => true).split(':').last
-    return unless local_logins
-    
-    local_logins = local_logins.strip.split(',')
-    logins_to_delete = local_logins - attributes.collect { |a| a['login'] }
-    logins_to_delete.map { |login| ShellUser.new('login' => login).destroy! }
-  end
-  
-  def self.data_for(project)
-    uri = URI.parse("http://keymaster.envylabs.com/projects/#{project}/users.yaml")
-    response = Net::HTTP.start(uri.host, uri.port) do |http|
+  ##
+  # Returns the data (body) from GET requesting the given URL.
+  # 
+  def self.query(url, options = {})
+    uri       = URI.parse(url)
+    response  = Net::HTTP.start(uri.host, uri.port) do |http|
+      yield(http) if block_given?
       http.get(uri.path)
     end
     
-    log("Aborting.", :fail => true) unless response_valid?(response)
+    unless response.kind_of?(Net::HTTPSuccess)
+      log("Non-200 Server Response - Code #{response.code}.", :fail => true)
+    end
     
-    unless current_version?(response['Api-Version'])
+    unless valid?(response.body, Base64.decode64(CGI.unescape(response['Response-Signature']))) || options[:ignore_signature]
+      log("Invalid signature received. Aborting.", :fail => true)
+    end
+    
+    unless current?(response['Api-Version']) || options[:ignore_version]
       log("Local version out-of-date, downloading and aborting.")
-      download_new_version!
+      update!
       exit(0)
     end
     
     response.body
+  rescue *HTTP_ERRORS
+    log("HTTP Error occurred: #{$!.class.name} - #{$!.message}", :fail => true)
   end
   
-  def self.response_valid?(net_http_response)
-    unless net_http_response.kind_of?(Net::HTTPSuccess)
-      log("Non-200 Server Response - Code #{net_http_response.code}.")
-      return false
-    end
-    
-    unless OpenSSL::PKey::RSA.new(PublicKey).
-      verify(OpenSSL::Digest::SHA256.new, Base64.decode64(CGI.unescape(net_http_response['Response-Signature'])), net_http_response.body)
-      log("Invalid signature received.")
-      return false
-    end
-    
-    true
-  end
-
-  attr_accessor :login, :full_name, :public_key, :uid
-
-  def initialize(attributes = {})
-    self.login      = attributes['login']
-    self.full_name  = attributes['full_name']
-    self.public_key = attributes['public_ssh_key']
-    self.uid        = attributes['uid']
-  end
-
-  def setup!
-    create! unless exists?
-    add_authorized_key! unless has_authorized_key?
-  end
-
-  def exists?
-    execute("egrep", :parameters => "-q ^#{self.login} /etc/passwd").success?
-  end
-
-  def create!
-    log(%|Creating "#{self.login}" user|)
-    execute("useradd", :parameters => "--groups sudo,envylabs_accounts --create-home --shell /bin/bash --uid #{self.uid} --comment \"#{self.full_name}\" --password \`dd if=/dev/urandom count=1 2> /dev/null | sha512sum | cut -c-128\` #{self.login}", :fail => true)
+  ##
+  # Returns +true+ if the given version matches the locally compatible 
+  # Keymaster.version.
+  # 
+  def self.current?(version)
+    Keymaster.version == version
   end
   
-  def destroy!
-    log(%|Destroying "#{self.login}" user|)
-    execute("killall", :parameters => %|-u "#{self.login}"|)
-    sleep(2) # allow the user to be logged out
-    execute("userdel", :parameters => %|-rf "#{self.login}"|, :fail => true)
+  ##
+  # Downloads the newest version of the Gatekeeper from the server and 
+  # overwrites the local installation.
+  # 
+  def self.update!
+    data = query("http://keymaster.envylabs.com/gatekeeper.rb", :ignore_version => true)
+    File.open(File.expand_path(__FILE__), 'w') { |f| f.write data }
+    log("Gatekeeper updated.")
   end
+  
+end
 
-  def has_authorized_key?
-    return false unless File.exist?(authorized_keys_path)
-    execute("grep", :parameters => %|-q "#{self.public_key}" "#{authorized_keys_path}"|).success?
+module LocalMachine
+  
+  module ExecutionResult
+
+    def success?
+      @success || false
+    end
+
+    def success=(value)
+      @success = value
+    end
+
   end
-
-  def add_authorized_key!
-    execute("mkdir", :parameters => %|-p "#{home_path}/.ssh"|, :fail => true)
-    execute("touch", :parameters => %|"#{authorized_keys_path}"|, :fail => true)
-    chown_home
-    execute("echo", :parameters => %|#{self.public_key} > "#{authorized_keys_path}" && chmod 0600 "#{authorized_keys_path}"|, :fail => true)
+  
+  ##
+  # Execute a file on the server with optional parameters.
+  # 
+  def self.execute(executable, options = {})
+    executable_path = `/usr/bin/env which "#{executable}"`.strip
+    if $?.success?
+      result = `\"#{executable_path}\" #{options[:parameters]}`.strip
+      result.extend(ExecutionResult)
+      result.success = $?.success?
+      log(%|Execution of "#{executable_path}" #{options[:parameters]} failed|, :fail => options[:fail]) unless result.success?
+      result
+    else
+      log(%|Could not locate "#{executable}" in the user's environment|, :fail => true)
+    end
   end
-
-  def chown_home
-    execute("chown", :parameters => %|-R #{login}:#{login} "#{home_path}"|, :fail => true)
+  
+  ##
+  # Log a message to syslog.
+  # 
+  def self.log(message, options = {})
+    puts message
+    execute("logger", :parameters => %|-i -t "Gatekeeper" "#{message}"|)
+    exit(1) if options[:fail]
   end
-
-  def home_path
-    "/home/#{login}"
-  end
-
-  def authorized_keys_path
-    "#{home_path}/.ssh/authorized_keys"
+  
+  def self.setup!
+    add_sudo_group
+    set_sudo_nopasswd
+    add_envylabs_group
   end
   
   
   private
   
   
-  def self.current_version?(version)
-    Version == version
-  end
-  
-  def self.download_new_version!
-    uri = URI.parse("http://keymaster.envylabs.com/gatekeeper.rb")
-    response = Net::HTTP.start(uri.host, uri.port) do |http|
-      http.get(uri.path)
+  ##
+  # Check for the sudo group, add if it doesn't exist.
+  #
+  def self.add_sudo_group
+    unless execute("egrep", :parameters => %|-q ^sudo /etc/group|).success?
+      execute("groupadd", :parameters => "sudo", :fail => true)
     end
-    
-    log("Aborting.", :fail => true) unless response_valid?(response)
-    
-    File.open(File.expand_path(__FILE__), 'w') { |f| f.write response.body }
-    log("Gatekeeper updated.")
+  end
+
+  ##
+  # Check for correct sudoer line, add if it doesn't exist.
+  #
+  def self.set_sudo_nopasswd
+    unless execute("grep", :parameters => %|-q "%sudo   ALL=NOPASSWD: ALL" /etc/sudoers|).success?
+      execute("echo", :parameters => %|"%sudo   ALL=NOPASSWD: ALL" >> /etc/sudoers|, :fail => true)
+    end
+  end
+
+  ##
+  # Check for the envylabs group, add if it doesn't exist.
+  #
+  def self.add_envylabs_group
+    unless execute("egrep", :parameters => %|-q ^envylabs_accounts /etc/group|).success?
+      execute("groupadd", :parameters => %|envylabs_accounts|, :fail => true)
+    end
   end
   
 end
+
+##
+# Envy Labs ShellUsers
+#
+class ShellUser
+  
+  attr_accessor :login, :full_name, :public_key, :uid
+  
+  
+  ##
+  # Synchronizes shell users on the system with those dictated by the 
+  # Keymaster server for the requested project.
+  # 
+  # This also synchronizes the 
+  # 
+  def self.synchronize(project)
+    users = Keymaster.users_for_project(project)
+    add_users(users)
+    remove_unlisted_users(users)
+    
+    new({
+      :login      => 'deploy',
+      :full_name  => 'Application Deployment User',
+      :public_ssh_key  => users.collect { |u| u.public_key }
+    }, {
+      :groups     => 'webapps'
+    }).setup!
+  end
+  
+  
+  def initialize(attributes = {}, options = {})
+    self.login      = attributes['login']           || attributes[:login]
+    self.full_name  = attributes['full_name']       || attributes[:full_name]
+    self.public_key = attributes['public_ssh_key']  || attributes[:public_ssh_key]
+    self.uid        = attributes['uid']             || attributes[:uid]
+    @groups         = options[:groups]
+  end
+
+  def setup!
+    create! unless exists?
+    synchronize_authorized_keys!
+  end
+
+  def destroy!
+    log(%|Destroying "#{self.login}" user|)
+    execute("killall", :parameters => %|-u "#{self.login}"|)
+    sleep(2) # allow the user to be logged out
+    execute("userdel", :parameters => %|-rf "#{self.login}"|, :fail => true)
+  end
+  
+  def synchronize_authorized_keys!
+    keys = [public_key].flatten
+    
+    execute("mkdir", :parameters => %|-p "#{home_path}/.ssh"|, :fail => true)
+    execute("touch", :parameters => %|"#{authorized_keys_path}"|, :fail => true)
+    chown_home
+    
+    data = File.read(authorized_keys_path).strip
+    data = data.gsub(%r{#{Regexp.escape(comment_open)}.*?#{Regexp.escape(comment_close)}}m, '').strip
+    keys.each { |key| data = data.gsub(%r{#{Regexp.escape(key)}}, '') } # temporarily, explicitly remove keys left over after stripping header block.  This is because existing envylabs users will have authorized keys outside of the block (set before this was well managed).
+    data << "\n#{comment_open}"
+    keys.each { |key| data << "\n#{key}" }
+    data << "\n#{comment_close}"
+    data << "\n"
+    File.open(authorized_keys_path, 'w') { |file| file.write data }
+  end
+  
+  
+  private
+  
+  
+  def self.add_users(users)
+    users.each { |user| user.setup! }
+  end 
+  
+  def self.remove_unlisted_users(users)
+    local_logins = execute("cat", :parameters => %|/etc/group \| grep "^envylabs_accounts"|, :fail => true).split(':').last
+    return unless local_logins
+    local_logins = local_logins.strip.split(',')
+    local_logins = local_logins - users.collect { |u| u.login }
+    local_logins.each { |login| new('login' => login).destroy! }
+  end
+  
+  
+  def exists?
+    execute("egrep", :parameters => "-q ^#{self.login} /etc/passwd").success?
+  end
+  
+  def create!
+    log(%|Creating "#{self.login}" user|)
+    execute("useradd", :parameters => "--groups #{@groups || 'sudo,envylabs_accounts'} --create-home --shell /bin/bash #{uid ? "--uid #{self.uid}" : ''} --comment \"#{self.full_name}\" --password \`dd if=/dev/urandom count=1 2> /dev/null | sha512sum | cut -c-128\` #{self.login}", :fail => true)
+  end
+  
+  def chown_home
+    execute("chown", :parameters => %|-R #{login}:#{login} "#{home_path}"|, :fail => true)
+  end
+  
+  def home_path
+    "/home/#{login}"
+  end
+  
+  def authorized_keys_path
+    "#{home_path}/.ssh/authorized_keys"
+  end
+  
+  def comment_open
+    '# Begin Gatekeeper generated keys'
+  end
+  
+  def comment_close
+    '# End Gatekeeper generated keys'
+  end
+  
+end
+
+
+def log(message, options = {})
+  LocalMachine.log(message, options)
+end
+
+def execute(executable, options = {})
+  LocalMachine.execute(executable, options)
+end
+
 
 ##
 # Need to run as root/sudo
@@ -221,25 +347,5 @@ if ENV['PROJECT'].nil? || ENV['PROJECT'] == ''
   log("Please specify the project", :fail => true)
 end
 
-##
-# Check for the sudo group, add if it doesn't exist.
-#
-unless execute("egrep", :parameters => %|-q ^sudo /etc/group|).success?
-  execute("groupadd", :parameters => "sudo", :fail => true)
-end
-
-##
-# Check for correct sudoer line, add if it doesn't exist.
-#
-unless execute("grep", :parameters => %|-q "%sudo   ALL=NOPASSWD: ALL" /etc/sudoers|).success?
-  execute("echo", :parameters => %|"%sudo   ALL=NOPASSWD: ALL" >> /etc/sudoers|, :fail => true)
-end
-
-##
-# Check for the envylabs group, add if it doesn't exist.
-#
-unless execute("egrep", :parameters => %|-q ^envylabs_accounts /etc/group|).success?
-  execute("groupadd", :parameters => %|envylabs_accounts|, :fail => true)
-end
-
-ShellUser.manage(ENV['PROJECT'].to_s.strip.downcase)
+LocalMachine.setup!
+ShellUser.synchronize(ENV['PROJECT'].to_s.strip.downcase)
