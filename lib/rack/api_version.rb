de944f55e8796717cd91febc59ee9373fd1eb6db
Nathaniel Bibler <nate@envylabs.com>
2011-04-03 22:25:37 -0400

    Pass API version as an optional middleware variable

--- lib/rack/api_version.rb
+++ lib/rack/api_version.rb
@@ -1,12 +1,13 @@
 module Rack
   class ApiVersion
-    def initialize(app, options = {})
+    def initialize(app, version = Keymaster.version)
       @app = app
+      @version = version
     end
 
     def call(env)
       @app.call(env).tap do |response|
-        response[1]['X-API-Version'] = Keymaster.version
+        response[1]['X-API-Version'] = @version
       end
     end
   end
