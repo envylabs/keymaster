6ff98b8305b86356ff13bbfae491a62ea8c5d9cf
Nathaniel Bibler <nate@envylabs.com>
2013-08-08 22:09:52 -0400

    Remove first! shim.

--- config/initializers/find_first_bang.rb
+++
@@ -1,9 +0,0 @@
-module FindFirstBang
-  def first!(*args)
-    first || raise(ActiveRecord::RecordNotFound, "There are no #{name} instances to return")
-  end
-end
-
-unless ActiveRecord::Base.methods.include?(:first!)
-  ActiveRecord::Base.extend FindFirstBang
-end
