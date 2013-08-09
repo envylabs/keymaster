e7d0ede86093940568243cb532cba5936c754c50
Nathaniel Bibler <nate@envylabs.com>
2013-08-08 22:15:54 -0400

    Update the interface for the YAML renderer.
    This matches the change introduced with https://github.com/rails/rails/commit/4f044528c0373b0a581a12b1311a7a544ded6c58.

--- config/initializers/yaml_renderer.rb
+++ config/initializers/yaml_renderer.rb
@@ -1,6 +1,6 @@
 require 'yaml'
 
-ActionController::Renderers.add :yaml do |yaml, options|
+ActionController::Renderers.add(:yaml) do |yaml, options|
   self.content_type ||= Mime::YAML
-  self.response_body = yaml.respond_to?(:to_yaml) ? yaml.to_yaml(options) : yaml
+  yaml.respond_to?(:to_yaml) ? yaml.to_yaml(options) : yaml
 end
