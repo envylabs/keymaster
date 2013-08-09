02970504e2969e9f6a66c62c1e1b5a20f26d017f
Nathaniel Bibler <nate@envylabs.com>
2013-08-08 23:08:47 -0400

    Move secret token into an environment variable.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Keymaster::Application.config.secret_token = 'a29128707789ffbd5327ada1a5ce0de5f96fd283c0cf81642f21b3873f33208490ad546ec70b3d6d59a481b4f9ad73a593605e24ac2d5af5b3a90c64fae7c346'
+Keymaster::Application.config.secret_token = ENV["SECRET_TOKEN"]
